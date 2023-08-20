0e93b95b8c3cde29a78d9840b9c61a9f3b43c6b1
Rag0603 <142712532+Rag0603@users.noreply.github.com>
2023-08-20 12:23:17 +0530

    Add files via upload

---
+++ day1/HelloWorld.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		int age = 25;
+		boolean attendTheClass = true;
+		String carName = "Hyundai";
+		
+		System.out.println(25);
+		System.out.println("Did you attend the class today? "+ attendTheClass );
+		System.out.println("The Brand is: "+ carName);
+		
+	}
+}
