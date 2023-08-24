453361fdccfdb5a9ad2d34313d560de8f2aba123
Rag0603 <142712532+Rag0603@users.noreply.github.com>
2023-08-23 21:18:17 -0500

    Add files via upload

---
+++ day1/CheckPositiveOrNegative.java
@@ -0,0 +1,33 @@
+package Assignments;
+
+public class CheckPositiveOrNegative {
+
+	
+public static void main(String[] args) {
+		
+		
+		int a= 6;
+		if (a<0) {
+			
+			
+			System.out.println("The Give number " + a + " is a Negative number");
+			
+		}else if (a==0) {
+			
+			System.out.println(a + " is neither a positive number nor a negative number, it's a neutral");
+		}
+		
+		
+		else {
+			
+			System.out.println("The Give number " + a + " is a positive number" );
+		}
+	}
+	
+}
+
+
+
+	
+	
+	
